b1871e05d469d4f01fe9094145900bf768849f63
Jacob Brasil <jnxtbrasil@gmail.com>
2020-09-23 19:54:05 -0400

    Switching to GatsbyJS

--- routes/contact.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// Serve Home
-router.get('/contact', (req, res) => {
-	res.redirect('mailto:jnxtbrasil@gmail.com?subject=👋%20Hey%20Jacob!')
-})
-
-module.exports = router;
