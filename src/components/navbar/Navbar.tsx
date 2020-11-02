95d19f64ea89e598b1c44750430cdc579f7b3ade
Jacob Brasil <jnxtbrasil@gmail.com>
2020-11-01 21:51:24 -0500

    Navbar color can be changed

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,20 +4,20 @@ import styled from "styled-components"
 import Logo from "./Logo"
 
 const Navigation = styled.nav`
-  background-color: transparent;
   position: relative;
   text-transform: uppercase;
   z-index: 2;
   align-self: center;
   border: none;
   height: 2.5rem;
-  margin-top: 3.125rem;
+  padding-top: 3.125rem;
+  padding-bottom: 2rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
 
   @media (max-width: 768px) {
-    margin-top: 1rem;
+    padding-top: 1rem;
     top: 0;
     left: 0;
     right: 0;
@@ -87,25 +87,74 @@ const Hamburger = styled.div`
   }
 `
 
-const Navbar = ({ siteTitle, menuLinks }) => {
+const HamburgerDark = styled.div`
+  background-color: #0f0f11;
+  width: 30px;
+  height: 3px;
+  transition: all 0.3s linear;
+  align-self: center;
+  justify-self: flex-end;
+  position: relative;
+  transform: ${props => (props.open ? "rotate(-45deg)" : "inherit")};
+
+  ::before,
+  ::after {
+    width: 30px;
+    height: 3px;
+    background-color: #0f0f11;
+    content: "";
+    position: absolute;
+    transition: all 0.3s linear;
+  }
+
+  ::before {
+    transform: ${props =>
+      props.open ? "rotate(-90deg) translate(-10px, 0px)" : "rotate(0deg)"};
+    top: -10px;
+  }
+
+  ::after {
+    opacity: ${props => (props.open ? "0" : "1")};
+    transform: ${props => (props.open ? "rotate(90deg) " : "rotate(0deg)")};
+    top: 10px;
+  }
+`
+
+const Navbar = ({ siteTitle, menuLinks, useDark = false }) => {
   const [navbarOpen, setNavbarOpen] = useState(false)
 
   return (
     <Navigation>
-      <Logo />
+      <Logo siteTitle={siteTitle} textColor={useDark ? "#0F0F11" : "#f7f7ff"} />
       <Toggle
         navbarOpen={navbarOpen}
         onClick={() => setNavbarOpen(!navbarOpen)}
       >
-        {navbarOpen ? <Hamburger open /> : <Hamburger />}
+        {navbarOpen ? (
+          useDark ? (
+            <HamburgerDark open />
+          ) : (
+            <Hamburger open />
+          )
+        ) : useDark ? (
+          <HamburgerDark />
+        ) : (
+          <Hamburger />
+        )}
       </Toggle>
       {navbarOpen ? (
         <Navbox>
-          <NavbarLinks menuLinks={menuLinks} />
+          <NavbarLinks
+            menuLinks={menuLinks}
+            textColor={useDark ? "#0F0F11" : "#f7f7ff"}
+          />
         </Navbox>
       ) : (
         <Navbox open>
-          <NavbarLinks menuLinks={menuLinks} />
+          <NavbarLinks
+            menuLinks={menuLinks}
+            textColor={useDark ? "#0F0F11" : "#f7f7ff"}
+          />
         </Navbox>
       )}
     </Navigation>
